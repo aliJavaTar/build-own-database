4fe190b4209370b96cb9d4befbaf946588aba60a
aliJavaTar <ali77erfagh99@gmail.com>
2026-05-30 12:07:39 +0330

    add logs

---
+++ build_our_database/src/main/java/com/own/log/AppendLog.java
@@ -0,0 +1,32 @@
+package com.own.log;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class AppendLog {
+    public FileChannel openLog(String path) throws IOException {
+        Path logPath = Paths.get(path);
+        return FileChannel.open(logPath, StandardOpenOption.READ,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.CREATE);
+    }
+
+    public void appendLog(FileChannel channel, String line) throws IOException {
+        try {
+            String record = line + "\n";
+            byte[] bytes = record.getBytes(StandardCharsets.UTF_8);
+            channel.write(ByteBuffer.wrap(bytes));
+            channel.force(true);
+
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        } finally {
+            channel.close();
+        }
+    }
+}
